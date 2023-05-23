870d59fd74b425263676b418524e15a32240e427
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2023-05-23 17:03:03 +0100

    empresa

---
+++ models/tb_documento.js
@@ -0,0 +1,35 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const conn = require('../db/conn')
+
+const Documento = conn.define('tb_documento', {
+    id_documento: {
+    type:DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+
+  empresa: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  tipo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  vencimento: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  observacao: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  pdf: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  
+},{ freezeTableName: true });
+
+module.exports = Documento;
---
+++ models/tb_empresa.js
@@ -0,0 +1,47 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const conn = require('../db/conn')
+
+const Empresa = conn.define('tb_empresa', {
+    id_empresa: {
+    type:DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+
+  razao: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cnpj: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  endereco: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  telefone1: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  telefone2: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  responsavel: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  
+},{ freezeTableName: true });
+
+module.exports = Empresa;
