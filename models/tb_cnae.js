497a812dd5c7757925cdcb723bbfcca9d20cfca8
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2023-05-23 16:49:23 +0100

    conn

---
+++ models/tb_cnae.js
@@ -0,0 +1,19 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const conn = require('../db/conn')
+
+const Cnae = conn.define('tb_cnae', {
+    id_cnae: {
+    type:DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+
+  cnae: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  
+},{ freezeTableName: true });
+
+module.exports = Cnae;
